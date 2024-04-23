f87c8bfc218725aaaef8b2ee52ab88c50cd02e4d
Samuel Åkesson <samak519@student.liu.se>
2024-04-24 00:27:30 +0200

    build simple map in Tiled

---
+++ assets/frame1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="frame1" tilewidth="12" tileheight="12" tilecount="100" columns="10">
+ <image source="build/frame1.png" width="120" height="120"/>
+</tileset>
