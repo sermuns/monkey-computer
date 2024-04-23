010ec1008e8f4d998b9f70b75bfacb73e9d6ae4d
Samuel Åkesson <samak519@student.liu.se>
2024-04-23 14:18:34 +0200

    working script to export collor palette and frames, tiles from LibreSprite project.

---
+++ assets/generate_palette.py
@@ -0,0 +1,158 @@
+from PIL import Image
+
+def generate_possible_colors(bits):
+    """
+    Return list of all possible hex colors with the given amount of bits per
+    color channel.
+    """
+
+    # Calculate the number of possible hues per channel
+    hues = 2**bits
+
+    # Create a list of possible colors
+    colors = []
+    for r in range(hues):
+        for g in range(hues):
+            for b in range(hues):
+                # map to 8-bit color
+                r_mapped = int(round(r/(hues-1) * 255)) if hues > 1 else 0
+                g_mapped = int(round(g/(hues-1) * 255)) if hues > 1 else 0
+                b_mapped = int(round(b/(hues-1) * 255)) if hues > 1 else 0
+
+                # Convert to hex
+                r_hex = hex(r_mapped)[2:].zfill(2)
+                g_hex = hex(g_mapped)[2:].zfill(2)
+                b_hex = hex(b_mapped)[2:].zfill(2)
+
+                # Concatenate the hex values
+                colors.append(f'{r_hex}{g_hex}{b_hex}')
+
+    # sort the list by hue
+    colors.sort()
+
+    return colors
+
+
+def sort_by_hue(colors, starting_hue=0):
+    """
+    Given a list of hex colors, return a list of colors sorted by hue.
+    """
+
+    # Create a list of tuples with the hue and the color
+    hues = []
+    for color in colors:
+        r = int(color[:2], 16)
+        g = int(color[2:4], 16)
+        b = int(color[4:], 16)
+
+        # Calculate the hue
+        max_color = max(r, g, b)
+        min_color = min(r, g, b)
+
+        if max_color == min_color:
+            hue = 0
+        elif max_color == r:
+            hue = (g - b) / (max_color - min_color)
+        elif max_color == g:
+            hue = 2 + (b - r) / (max_color - min_color)
+        else:
+            hue = 4 + (r - g) / (max_color - min_color)
+
+        # Shift the hue by the starting hue
+        hue = (hue + starting_hue) % 6
+
+        hues.append((hue, color))
+
+    # Sort the list by hue
+    hues.sort()
+
+    # Return the sorted colors
+    return [color for hue, color in hues]
+
+
+def save_to_image(palette, image_name):
+    """
+    Given a list of hex colors, save them to a flat image.
+    """
+
+    # save to an image with height 1px and width equal to the number of colors
+    image = Image.new('RGB', (len(palette), 1))
+
+    # fill with the colors
+    pixels = image.load()
+
+    for i, color in enumerate(palette):
+        r = int(color[:2], 16)
+        g = int(color[2:4], 16)
+        b = int(color[4:], 16)
+
+        pixels[i, 0] = (r, g, b)
+
+    image.save(image_name)
+
+
+def move_black_and_white_to_front(palette):
+    """
+    Given a list of hex colors, move the black and white colors to the front.
+    """
+
+    # remove the black and white colors
+    palette.remove('000000')
+    palette.remove('ffffff')
+
+    # move them to the front
+    palette.insert(0, '000000')
+    palette.insert(0, 'ffffff')
+
+    return palette
+    
+
+def get_list_of_colors(image_name : str) -> list:
+    """
+    Given an image, return a list of all present colors in hex format.
+    """
+
+    # Load the image
+    image = Image.open(image_name)
+
+    # Get the size of the image
+    width, height = image.size
+
+    # Get the pixel data
+    pixels = image.load()
+
+    # Create a set of unique colors
+    colors = set()
+
+    # Add each color to the set
+    for x in range(width):
+        for y in range(height):
+            pixel = pixels[x, y]
+            # convert to hex
+            r_hex = hex(pixel[0])[2:].zfill(2)
+            g_hex = hex(pixel[1])[2:].zfill(2)
+            b_hex = hex(pixel[2])[2:].zfill(2)
+            
+            colors.add(f'{r_hex}{g_hex}{b_hex}')
+
+    return list(colors)
+
+
+def main():
+    possible_colors = generate_possible_colors(2)
+    sorted_colors = sort_by_hue(possible_colors, 100)
+    palette = move_black_and_white_to_front(sorted_colors)
+    save_to_image(palette, 'palette.gif')
+
+
+if __name__ == '__main__':
+    # save_to_image(get_list_of_colors('build/frame1.png'), 'palette.gif')
+    colors_in_frame = get_list_of_colors('build/frame3.png')
+    print(colors_in_frame)
+    save_to_image(colors_in_frame, 'palette.gif')
+    
+
+
+    
+
+    
--- assets/png_to_hex.py
+++ assets/png_to_hex.py
@@ -1,9 +1,11 @@
 import os, sys
 from PIL import Image
 
-def png_to_hex(image_path):
+BINARY_WIDTH = 3
+
+def png_to_bin(image_path):
     """
-    Given an image, return an array of hexadecimal values representing
+    Given an image, return an array of binary values representing
     the color of each pixel in the image. 
     """
 
@@ -18,8 +20,8 @@ def png_to_hex(image_path):
     # Get the pixel data
     pixels = image.load()
 
-    # Convert each pixel to hex
-    hex_values = []
+    # Convert each pixel to binary
+    binary_values = []
     for y in range(height):
         for x in range(width):
             r, g, b, a = pixels[x, y]
@@ -27,38 +29,46 @@ def png_to_hex(image_path):
             if a < 100: # Transparent enough
                 r = g = b = 255 # MAKE IT COMPLETELY WHITE => TRANSPARENT
 
-            # Map to 4-bit color
-            r = r // 16
-            g = g // 16
-            b = b // 16
-            a = a // 16
+            # Map to 3-bit color
+            r = int(round(r/255 * (2**BINARY_WIDTH - 1)))
+            g = int(round(g/255 * (2**BINARY_WIDTH - 1)))
+            b = int(round(b/255 * (2**BINARY_WIDTH - 1)))
 
-            # Add to the list
-            hex_values.append(f'{r:x}{g:x}{b:x}')
+            # Convert to binary
+            r_binary = bin(r)[2:].zfill(BINARY_WIDTH)
+            g_binary = bin(g)[2:].zfill(BINARY_WIDTH)
+            b_binary = bin(b)[2:].zfill(BINARY_WIDTH)
 
-    return hex_values
+            # Concatenate the binary values
+            binary_values.append(f'{r_binary}{g_binary}{b_binary}')
+
+    print(binary_values)
+    return binary_values
 
 
-def hex_to_png(hex_values):
+def bin_to_image(bin_values):
     """
-    Given an array of hexadecimal values, retrn an image from the values.
+    Given an array of binary values representing the color of each pixel in an image,
+    return a new image.
     """
-
-    # Use hex_values to create an image
+    
+    # Create a new image
     new_image = Image.new('RGB', (12, 12))
 
-    # Get the pixel data
+    # Load the pixel data
     pixels = new_image.load()
-    
-    # Convert each hex value to RGB
-    for y in range(12):
-        for x in range(12):
-            hex_value = hex_values.pop(0)
-            r = int(hex_value[0], 16) * 16
-            g = int(hex_value[1], 16) * 16
-            b = int(hex_value[2], 16) * 16
 
-            pixels[x, y] = (r, g, b)
+    # Convert each binary value to RGB
+    for i, binary_value in enumerate(bin_values):
+        r_binary = binary_value[:BINARY_WIDTH]
+        g_binary = binary_value[BINARY_WIDTH:2*BINARY_WIDTH]
+        b_binary = binary_value[2*BINARY_WIDTH:]
+
+        r = int(r_binary, 2) * 255 // (2**BINARY_WIDTH - 1)
+        g = int(g_binary, 2) * 255 // (2**BINARY_WIDTH - 1)
+        b = int(b_binary, 2) * 255 // (2**BINARY_WIDTH - 1)
+
+        pixels[i % 12, i // 12] = (r, g, b)
 
     return new_image
 
@@ -86,15 +96,15 @@ def main():
 
     image_path = sys.argv[1]
 
-    hex_values = png_to_hex(image_path)
+    bin_values = png_to_bin(image_path)
 
     # to vhdl
-    vhdl_array_elements = hex_values_to_vhdl_array_elements(hex_values)
-    print(vhdl_array_elements)
+    # vhdl_array_elements = hex_values_to_vhdl_array_elements(bin_values)
+    # print(vhdl_array_elements)
 
     # test show file
-    # new_image = hex_to_png(hex_values)
-    # new_image.show('new_image.png')
+    new_image = bin_to_image(bin_values)
+    new_image.show('new_image.png')
 
     
 if __name__ == '__main__':
