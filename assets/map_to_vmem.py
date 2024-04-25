d7e3b0504b8d8bb4b7c1f896a6030c642287d72d
Samuel Åkesson <samak519@student.liu.se>
2024-04-25 23:37:49 +0200

    working import from Tiled map to vmem

--- assets/map_to_vmem.py
+++ assets/map_to_vmem.py
@@ -1,8 +1,13 @@
 """
-Parse a map file from Tiled and export it as a VHDL array
+Parse a map file from Tiled and export it as VHDL array elements into
+video memory file.
 """
 
 import xml.etree.ElementTree as ET
+import sys
+import re
+
+VMEM_HEIGHT = 25  # currently 25 lines in pMem.vhd
 
 
 def read_tmx_file(file_path):
@@ -29,7 +34,8 @@ def get_final_map(layers):
     for layer in layers[::-1]:
         data = layer.find("data")
         rows = data.text.split("\n")
-        rows = [row.removesuffix(",") for row in rows if row != ""] # remove empty rows
+        rows = [row.removesuffix(",")
+                for row in rows if row != ""]  # remove empty rows
         rows = [[int(tile) for tile in row.split(",")] for row in rows]
 
         # add the tiles to the map
@@ -41,20 +47,86 @@ def get_final_map(layers):
 
             # check if the cell is empty
             for j, tile in enumerate(row):
-                if map[i][j] == 0: # can overwrite
+                if map[i][j] == 0:  # can overwrite
                     map[i][j] = tile
 
     return map
 
 
+def create_vmem_line(index: int, tiles: list) -> str:
+    """
+    Create a correct VHDL line for the video memory.
+    Every tiletype is represented as unsigned 6-bit integer.
+    Each line only fits 4 tiles.
+    """
+
+    if len(tiles) > 4:
+        raise ValueError("Too many tiles in a line")
+
+    line = f"        VMEM_START + {index:02} => b\""
+
+    for tile in tiles:
+        if tile > 2**6 - 1:
+            raise ValueError(f"Tile {tile} is too big")
+        line += f"{tile:06b}_"
+
+    line = line.removesuffix("_")
+    line += "\",\n"
+
+    return line
+
+
+def write_to_vmem(map, vmem_file):
+    """
+    Write a map (matrix of tile-types) to the vmem file
+    """
+
+    with open(vmem_file, "r") as f:
+        vmem_lines = f.readlines()
+
+    # find the line where the map starts
+    start_line = 0
+    for i, line in enumerate(vmem_lines):
+        if re.match(r'\s*VMEM_START\s*=>\s*b.*', line):
+            start_line = i
+            break
+
+    # clear previous contents of VMEM
+    vmem_lines[start_line:start_line+VMEM_HEIGHT] = []
+
+    # get flat map
+    flat_map = [tile for row in map for tile in row]
+
+    # write the map to the vmem file
+    for i in range(0, VMEM_HEIGHT):
+        vmem_line = create_vmem_line(i, flat_map[4*i:4*i+4])
+        vmem_lines.insert(start_line+i, vmem_line)
+
+    with open(vmem_file, "w") as f:
+        f.writelines(vmem_lines)
+
+
 def main():
+    if len(sys.argv) == 2 and sys.argv[1] == "--debug":
+        # for debugging purposes
+        sys.argv = ["map_to_vmem.py", "map.tmx", "../hardware/src/pMem.vhd"]
+    elif len(sys.argv) != 3:
+        print("Usage: python map_to_vmem.py <map.tmx> <pmem.vhd>")
+        sys.exit(1)
+
+    # unpack the arguments
+    _, map_file, vmem_file = sys.argv
+
     # get the root element
-    root = read_tmx_file("map.tmx")
+    root = read_tmx_file(map_file)
     # get the layers
     layers = root.findall("layer")
     # get the final map
     map = get_final_map(layers)
-    print(map)
+
+    # write the map to the vmem file
+    write_to_vmem(map, vmem_file)
+
 
 if __name__ == '__main__':
     main()
