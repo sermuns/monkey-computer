b82ee458a0200c129de1078b5481467b14c6341d
Samuel Åkesson <samak519@student.liu.se>
2024-05-05 20:37:42 +0200

    testing

--- masm/test.s
+++ masm/test.s
@@ -5,15 +5,16 @@ LDI GR3, 4
 LDI GR4, 5
 LDI GR5, 6
 LDI GR6, 7
-ST 1501, GR0
+
 LDI GR7, 9
 PUSH GR7
-ST 1502, GR1
-ST 1503, GR2
-ST 1504, GR3
-ST 1505, GR4
-ST 1506, GR5
-ST 1507, GR6
-RET --Check implementation sp might need to increment before last step. Should send us to  row 10
+
+ST 1500, GR1
+ST 1501, GR2
+ST 1502, GR3
+ST 1503, GR4
+ST 1504, GR5
+ST 1505, GR6
+@ RET --Check implementation sp might need to increment before last step. Should send us to  row 10
 
 
