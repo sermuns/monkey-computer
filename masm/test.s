12446b17deb87ee25370713a4bd5d592e1be4a4d
Zexie <danielalchasov03@gmail.com>
2024-05-05 13:01:29 +0200

    adding vid mem test to test.s

--- masm/test.s
+++ masm/test.s
@@ -1,2 +1,19 @@
-LDI GR0, 2
+LDI GR0, 1 --Loading different tiles into REGISTERS
+LDI GR1, 2
+LDI GR2, 3
+LDI GR3, 4
+LDI GR4, 5
+LDI GR5, 6
+LDI GR6, 7
 ST 1501, GR0
+LDI GR7, 9
+PUSH GR7
+ST 1502, GR1
+ST 1503, GR2
+ST 1504, GR3
+ST 1505, GR4
+ST 1506, GR5
+ST 1507, GR6
+RET --Check implementation sp might need to increment before last step. Should send us to  row 10
+
+
