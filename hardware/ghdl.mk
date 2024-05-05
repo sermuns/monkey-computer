b936e66236cf3c978d907b716509762c4096341e
Samuel Åkesson <samak519@student.liu.se>
2024-05-05 11:10:39 +0200

    refactor, rename and move targets

--- hardware/ghdl.mk
+++ hardware/ghdl.mk
@@ -3,6 +3,8 @@ WAVEDIR = wave
 SAVEDIR = save
 SCRIPTDIR = ../scripts
 GHDL_FLAGS = --std=08 --workdir=$(WORKDIR) 
+GHDL_RUN_FLAGS = --ieee-asserts=disable-at-0 --assert-level=error
+
 SRC_DIR=.
 
 # can be compiled in any order
@@ -20,21 +22,23 @@ ghelp:
 	@grep -E '^[a-zA-Z_-]+:' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":"}; {printf " \033[36m%-30s\033[0m\n", $$1}'
 
 parse_umem:
-	@python $(SCRIPTDIR)/parse_mem.py $(SRC_DIR)/uMem.vhd
+	python $(SCRIPTDIR)/parse_mem.py $(SRC_DIR)/uMem.vhd
  
 preprocess:
-	@python $(SCRIPTDIR)/preprocess.py -q
+	python $(SCRIPTDIR)/preprocess.py -q
 
 .PHONY: gclean
 gclean:
 	rm -rf $(WORKDIR) $(WAVEDIR)
 
+.PHONY: ghdl
 ghdl: $(WORKDIR)
-	@ghdl -a $(GHDL_FLAGS) $(SOURCE_FILES)
+	ghdl -a $(GHDL_FLAGS) $(SOURCE_FILES)
 
 $(WORKDIR):
 	@mkdir -p $(WORKDIR)
 
+.PHONY: gcompile
 gcompile: parse_umem preprocess ghdl
 
 %_tb.vhd: gcompile
@@ -42,7 +46,7 @@ gcompile: parse_umem preprocess ghdl
 	@ghdl -e $(GHDL_FLAGS) $*_tb
 	@mkdir -p $(WAVEDIR)
 # Allow for simulation to fail
-	-@ghdl -r $(GHDL_FLAGS) $*_tb --wave=$(WAVEDIR)/$*_tb.ghw --ieee-asserts=disable-at-0 --assert-level=error
+	-@ghdl -r $(GHDL_FLAGS) $*_tb --wave=$(WAVEDIR)/$*_tb.ghw $(GHDL_RUN_FLAGS)
 
 .PHONY: %.ghw
 %.ghw: ## Launch wave in GTKWave, if not already running
@@ -57,15 +61,14 @@ gtkwave: cpu_tb.vhd cpu_tb.ghw ## Simulate, then launch wave
 .PHONY: surfer
 surfer: cpu_tb.vhd
 	@if ! pgrep -x "surfer" > /dev/null; then \
-		surfer wave/cpu_tb.ghw -s save/cpu_tb.ron -c command/qol.surfer > /dev/null &
+		surfer wave/cpu_tb.ghw -s save/cpu_tb.ron -c command/qol.surfer > /dev/null & \
 	fi
 
-
-assemble: ## Assemble a program from src/masm into pMem.vhd
-	@if [ -z $(prog) ]; then \
-		echo "ERROR: No program given. Usage: make assemble prog=<name of program (without extension) in masm>"; \
-		exit 1; \
-	fi
-	@python3 $(SCRIPTDIR)/assembler.py $(prog)
+%.s: ## Assemble, then insert into program memory
+	python $(SCRIPTDIR)/assembler.py $*.s
 
 gsim_video: video_tb.vhd video_tb.ghw ## Simulate the video module
+
+.PHONY: emulate
+emulate:
+	@python $(SCRIPTDIR)/emulate.py &
