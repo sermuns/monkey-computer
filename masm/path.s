183379281c15c63c1fe34129ab9c0b668701b0a9
maxmw <maxwi609@student.liu.se>
2024-05-16 09:30:39 +0200

    animations and damage

--- masm/path.s
+++ masm/path.s
@@ -1,35 +1,107 @@
 %PROGRAM 0 1499
 start:
-    LDI GR2, 1
+    LDI GR2, 2
     STN %HEAP, GR2 // baloon spawn amount
-    LDI GR0, 35 // CONSTANT: balloon tiletype
+    LDI GR0, 34 //balloon tiletype
+    SUBI GR0, 1 //weird fix
+
+push_balloon_hp:
+    //hårdkodat (ska öka för varje dödad ballong tex)
+    LDI GR6, 4
+    PUSH GR6
     
 loop:
     STN %VMEM, GR1 // replace tiletype that was overwritten
     // find next path position
     MOV GR6, GR5
     SUBI GR6, 40
-    BEQ take_dmg
+    BEQ player_dmg
 new_ballon:
-    SUBI GR2, 0
-    BEQ dead
+
     MOV GR3, GR5 // GR3 := GR5
     LDN GR4, %PATH // GR4 := PATH[GR3]
     MOV GR3, GR4 // GR3 := GR4
                         
     LDN GR1, %VMEM // GR1 := VMEM[GR3]
-    STN %VMEM, GR0 // overwrite with balloon
+    //STN %VMEM, GR0 // overwrite with balloon
+    BRA balloon_animation
+check_monke:
     ADDI GR5, 1 // increment path index
-    BRA loop ;b
+    MOV GR4, GR3 //save balloon pos         CHECKAR BARA FÖR APA 1, borde checka 5,9,13 osv (starttillstånd inte animation)
 
+    ADDI GR3, 1 //right neighbour
+    LDN GR6, %VMEM
+    CMPI GR6, 1
+    BEQ monke_animation
+    //BEQ balloon_dmg
 
-take_dmg:
-    //TODO SUB players health
+    MOV GR3, GR4 //down
+    ADDI GR3, 10
+    LDN GR6, %VMEM
+    CMPI GR6, 1
+    BEQ monke_animation
+
+    MOV GR3, GR4 //left
+    SUBI GR3, 1
+    LDN GR6, %VMEM
+    CMPI GR6, 1
+    BEQ monke_animation
+
+    MOV GR3, GR4 //up
+    SUBI GR3, 10
+    LDN GR6, %VMEM
+    CMPI GR6, 1
+    BEQ monke_animation
+
+    MOV GR3, GR4
+    BRA loop
+
+player_dmg:
     LD GR2, %HEAP
     SUBI GR2, 1
     ST %HEAP, GR2
+    SUBI GR2, 0
+    BEQ dead
+
     LDI GR5, 0
     BRA new_ballon
+
+balloon_dmg:
+    LDI GR7, 1
+    STN %VMEM, GR7
+    MOV GR3, GR4
+    POP GR6
+    SUBI GR6, 1  //different damage for diff monkeys?????
+    BEQ balloon_dead
+    //otherwise push decreased health
+    PUSH GR6
+    BRA loop
+
+balloon_dead:
+    STN %VMEM, GR1
+    LDI GR5, 0
+    BRA push_balloon_hp
+
+monke_animation:
+    CMPI GR6, 4 //BEHÖVS DELAYS, BYTER ANNARS FÖR SNABBT
+    BEQ balloon_dmg
+    ADDI GR6, 1
+    STN %VMEM, GR6
+    BRA monke_animation ;b
+
+reset_anim_state:
+    SUBI GR0, 3
+    STN %VMEM, GR0
+    SUBI GR0, 1 ;b
+    BRA check_monke
+balloon_animation:
+    CMPI GR0, 37
+    BEQ reset_anim_state
+    ADDI GR0, 1
+    STN %VMEM, GR0
+    BRA balloon_animation ;b
+    
+
 
 dead:
 BRA dead ;b
@@ -57,9 +129,9 @@ BRA dead ;b
 0
 0
 25
+1
 0
-0
-0
+1
 0
 0
 0
